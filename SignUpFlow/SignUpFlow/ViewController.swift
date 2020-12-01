7224289fa3fc5dbfec94ca337e47d17f6c6d7c33
lina0322 <llim@student.42seoul.kr>
2020-12-01 20:35:34 +0900

    Feat : tabView(), idTextField, passwordTextField 생성

--- SignUpFlow/SignUpFlow/ViewController.swift
+++ SignUpFlow/SignUpFlow/ViewController.swift
@@ -8,11 +8,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tabView() {
+        
+    }
 
 }
 
