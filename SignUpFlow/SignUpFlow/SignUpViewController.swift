0a9c8dc8df7e37b4815c9aa9434038233639b9c8
lina0322 <llim@student.42seoul.kr>
2020-12-01 20:27:35 +0900

    Feat : dismissPage() in SignUpViewController

---
+++ SignUpFlow/SignUpFlow/SignUpViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpViewController.swift
+//  SignUpFlow
+//
+//  Created by 임리나 on 2020/12/01.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBAction func dissmissPage() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
