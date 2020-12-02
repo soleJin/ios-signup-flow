39f7bb4d939478ea8fce81c1ff7ab1cec67efb48
Lina.L <llim@student.42seoul.kr>
2020-12-02 09:58:41 +0900

    Revert "Step2 lina"

--- SignUpFlow/SignUpFlow/ViewController.swift
+++ SignUpFlow/SignUpFlow/ViewController.swift
@@ -6,52 +6,15 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
-    
-    @IBOutlet weak var idTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    
+class ViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        idTextField.delegate = self
-        passwordTextField.delegate = self
+       
     }
     
-    @IBAction func tabView() {
+    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
         self.view.endEditing(true)
     }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField == idTextField {
-            idTextField.resignFirstResponder()
-            passwordTextField.becomeFirstResponder()
-        } else {
-            passwordTextField.resignFirstResponder()
-            systemAlert()
-        }
-        return true
-    }
-    
-    @IBAction func touchUpSignIn(_ sender: UIButton) {
-        systemAlert()
-    }
-    
-    func systemAlert() {
-        if idTextField.text == "" {
-            showAlert(about: "아이디를 입력해주세요.")
-        } else if passwordTextField.text == "" {
-            showAlert(about: "비밀번호를 입력해주세요.")
-        } else {
-            showAlert(about: "로그인 기능이 비활성화 되어있습니다.")
-        }
-    }
-    
-    func showAlert(about message: String) {
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
-        
-        alert.addAction(okButton)
-        present(alert, animated: true, completion: nil)
-    }
 }
+
