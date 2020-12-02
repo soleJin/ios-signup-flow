a27b271823a48d3d8f0553be194f54dfc463d6f2
lina0322 <llim@student.42seoul.kr>
2020-12-02 09:39:16 +0900

    Style : viewController 주석 삭제 및 메서드 위치 변경

--- SignUpFlow/SignUpFlow/ViewController.swift
+++ SignUpFlow/SignUpFlow/ViewController.swift
@@ -7,14 +7,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
-        self.view.endEditing(true)
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+    }
+    
+    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
     }
 }
 
