f0da49c6f904970a233e0b905e7835d1ce7755bd
sole <subuchappyv2@gmail.com>
2020-12-03 13:50:55 +0900

    Rewind step2/step3

--- SignUpFlow/SignUpFlow/MainViewController.swift
+++ SignUpFlow/SignUpFlow/MainViewController.swift
@@ -8,59 +8,9 @@ import UIKit
 
 class MainViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var idTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var statusMessageLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        idTextField.delegate = self
-        passwordTextField.delegate = self
-        passwordTextField.isSecureTextEntry = true
-        changeLabel(text: statusMessageLabel, to: .empty)
     }
     
-    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
-        self.view.endEditing(true)
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField == idTextField {
-            passwordTextField.becomeFirstResponder()
-        } else {
-            passwordTextField.resignFirstResponder()
-            touchUpSignIn(nil)
-        }
-        return false
-    }
-    
-    @IBAction func touchUpSignIn(_ sender: UIButton?) {
-        guard isFullfill(textField: idTextField) else {
-            changeLabel(text: statusMessageLabel, to: .enterId)
-            return
-        }
-        
-        guard isFullfill(textField: passwordTextField) else {
-            changeLabel(text: statusMessageLabel, to: .enterPassword)
-            return
-        }
-        
-        changeLabel(text: statusMessageLabel, to: .disableSignIn)
-    }
-    
-    func changeLabel(text label: UILabel, to message: Message) {
-        label.text = message.rawValue
-    }
 }
 
-extension UIViewController {
-    func isFullfill(textField: UITextField...) -> Bool {
-        for text in textField {
-            guard text.text != "" else {
-                return false
-            }
-        }
-        return true
-    }
-}
--- SignUpFlow/SignUpFlow/SignUpViewController.swift
+++ SignUpFlow/SignUpFlow/SignUpViewController.swift
@@ -9,110 +9,11 @@ import UIKit
 
 class SignUpViewController: UIViewController {
     
-    @IBOutlet weak var idTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var checkPasswordField: UITextField!
-    @IBOutlet weak var profileImage: UIImageView!
-    @IBOutlet weak var introductionTextView: UITextView!
-    @IBOutlet weak var nextButton: UIButton!
-    
-    let imagePicker = UIImagePickerController()
-        
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let tapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(pickImage))
-     
-        profileImage.addGestureRecognizer(tapGestureRecognizer)
-        profileImage.isUserInteractionEnabled = true
-       
-        imagePicker.delegate = self
-        imagePicker.allowsEditing = true
-        
-        idTextField.delegate = self
-        passwordTextField.delegate = self
-        checkPasswordField.delegate = self
-        introductionTextView.delegate = self
-        
-        passwordTextField.isSecureTextEntry = true
-        checkPasswordField.isSecureTextEntry = true
-        
-        inactivateButton(nextButton)
     }
     
-    func checkCanGoNext() {
-        guard isFullfill(textField: idTextField, passwordTextField, checkPasswordField),
-              profileImage.image != nil,
-              introductionTextView.text != "" else {
-            return inactivateButton(nextButton)
-        }
-        
-        guard checkPasswordField.text == passwordTextField.text else {
-            passwordTextField.textColor = .red
-            checkPasswordField.textColor = .red
-            return inactivateButton(nextButton)
-        }
-        
-        passwordTextField.textColor = .black
-        checkPasswordField.textColor = .black
-        activateButton(nextButton)
-    }
-    
-    func activateButton(_ button: UIButton) {
-        nextButton.setTitleColor(.systemBlue, for: .normal)
-        nextButton.isEnabled = true
-    }
-    
-    func inactivateButton(_ button: UIButton) {
-        button.isEnabled = false
-        button.setTitleColor(.systemGray, for: .normal)
-    }
-    
-    @IBAction func dismissSignUpView() {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
-        self.view.endEditing(true)
-    }
 }
 
-extension SignUpViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
-    
-    @objc func pickImage() {
-        self.present(imagePicker, animated: true, completion: nil)
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
-            profileImage.image = image
-        }
-        checkCanGoNext()
-        dismiss(animated: true, completion: nil)
-    }
-}
 
-extension SignUpViewController: UITextViewDelegate, UITextFieldDelegate {
 
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        switch textField {
-        case idTextField:
-            passwordTextField.becomeFirstResponder()
-        case passwordTextField:
-            checkPasswordField.becomeFirstResponder()
-        case checkPasswordField:
-            introductionTextView.becomeFirstResponder()
-        default:
-            break
-        }
-        return true
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        checkCanGoNext()
-    }
-  
-    func textViewDidEndEditing(_ textView: UITextView) {
-        checkCanGoNext()
-    }
-}
